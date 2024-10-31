2928e6586fe84f4e0001b1ccd44edfcc09535850
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-10-31 17:20:39 +0700

    Add new routes for AnilistProfile, AnimeDetail, and AnimeSearch components

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,21 @@ import { createBrowserRouter } from "react-router-dom";
 import HomepageRoute from "./pages/Homepage/HomepageRouter";
 import MainLayout from "./layout/MainLayout";
 import AnilistGrantedRoute from "./pages/Anilist/Granted/AnilistGrantedRoute";
+import AnilistProfileRoute from "./pages/Anilist/Profile/AnilistProfileRouter";
+import AnimeDetailRoute from "./pages/Anime/Detail/AnimeDetailRouter";
+import AnimeSearchRoute from "./pages/Anime/Search/AnimeSearchRouter";
 
 const router = createBrowserRouter([
   AnilistGrantedRoute,
   {
     path: "/",
     element: <MainLayout />,
-    children: [HomepageRoute],
+    children: [
+      HomepageRoute,
+      AnilistProfileRoute,
+      AnimeDetailRoute,
+      AnimeSearchRoute,
+    ],
   },
 ]);
 
