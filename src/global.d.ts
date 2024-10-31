b157e87053283d272369676f9e1a2b3a4882f308
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-10-31 09:55:23 +0700

    Declare ongoingRequests global variable in global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,5 +3,6 @@ import type { AnilistHelper } from "./helper/AnilistHelper";
 declare global {
   interface Window {
     anilistHelper: AnilistHelper;
+    ongoingRequests: Map<string, AbortController>;
   }
 }
