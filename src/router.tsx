d8e8808510b156f73b9a2ef8a2012aaa9559b8d7
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-11-03 13:15:25 +0700

    Refactor AnilistProfile and AnimeDetail components

--- src/router.tsx
+++ src/router.tsx
@@ -2,8 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import HomepageRoute from "./pages/Homepage/HomepageRouter";
 import MainLayout from "./layout/MainLayout";
 import AnilistGrantedRoute from "./pages/Anilist/Granted/AnilistGrantedRoute";
-import AnilistProfileRoute from "./pages/Anilist/Profile/AnilistProfileRouter";
-import AnimeDetailRoute from "./pages/Anime/Detail/AnimeDetailRouter";
 import AnimeSearchRoute from "./pages/Anime/Search/AnimeSearchRouter";
 
 const router = createBrowserRouter([
@@ -11,12 +9,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [
-      HomepageRoute,
-      AnilistProfileRoute,
-      AnimeDetailRoute,
-      AnimeSearchRoute,
-    ],
+    children: [HomepageRoute, AnimeSearchRoute],
   },
 ]);
 
